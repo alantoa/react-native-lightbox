bd7d3560ef1f9217630ecbc10c281207d967defc
alantoa <496807117@qq.com>
2022-06-06 00:19:43 +0800

    fix: clamp method not found.

--- src/utils.tsx
+++ src/utils.tsx
@@ -1,6 +1,22 @@
 import type Animated from 'react-native-reanimated';
 import { useSharedValue } from 'react-native-reanimated';
-import { clamp } from 'react-native-redash';
+
+/**
+ *  @summary Clamps a node with a lower and upper bound.
+ *  @example
+    clamp(-1, 0, 100); // 0
+    clamp(1, 0, 100); // 1
+    clamp(101, 0, 100); // 100
+  * @worklet
+  */
+export const clamp = (
+  value: number,
+  lowerBound: number,
+  upperBound: number
+) => {
+  'worklet';
+  return Math.min(Math.max(lowerBound, value), upperBound);
+};
 
 export const rubberBandClamp = (x: number, coeff: number, dim: number) => {
   'worklet';
